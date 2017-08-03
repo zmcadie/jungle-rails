35becc56233b38dfdd8e4f4447eec7b05620f283
Zola <zolamcadie@gmail.com>
2017-08-03 19:15:26 +0000

    updates sessions_controller logic to allow login(create) and logout(destroy)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,22 @@
 class SessionsController < ApplicationController
 
   def new
+    @session = Session.new
   end
 
   def create
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      @session[:user_id] = @user.id
+      redirect_to [:products], notice: 'Logged in'
+    else
+      redirect_to [:sessions, :new]
+    end
   end
 
   def destory
+    @session[user_id] = nil
+    redirect_to [:sessions, :new]
   end
 
 end
