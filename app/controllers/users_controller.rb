ec55e03893c6204f240548c1f152debbe21d01a0
Zola <zolamcadie@gmail.com>
2017-08-03 19:06:42 +0000

    fixed bug in user controller (my typos broke things)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to [:products], notice: 'Thanks for registering!'
     else
       render :new
+    end
   end
 
   private
