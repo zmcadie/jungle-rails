2c40e367f389581157038eabf45e64852d81fc54
Zola <zolamcadie@gmail.com>
2017-08-03 18:51:01 +0000

    added logic to user_controller create action and to sanitize form input

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,28 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to [:products], notice: 'Thanks for registering!'
+    else
+      render :new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 
 end
