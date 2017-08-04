1d28b579f9e5a4ae6d330b395170f5ed3d8f3357
Zola <zolamcadie@gmail.com>
2017-08-04 02:26:30 +0000

    added validations to product reviews

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,9 +6,6 @@ class Product < ActiveRecord::Base
   belongs_to :category
   has_many :reviews
 
-  validates :name, presence: true
-  validates :price, presence: true
-  validates :quantity, presence: true
-  validates :category, presence: true
+  validates :name, :price, :quantity, :category, presence: true
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ActiveRecord::Base
   belongs_to :product
+
+  validates :product_id, :user_id, :rating, presence: true
 end
