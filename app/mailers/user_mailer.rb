a2049363d186df838ac0b9d60dad5577c3ca6c6d
Zola <zolamcadie@gmail.com>
2017-08-04 01:15:18 +0000

    implemented email receipt on order completion

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,8 @@ class UserMailer < ApplicationMailer
   default from: 'no-reply@jungle.com'
 
   def email_receipt(order)
-    mail(to: order.email, subject: "Order \##{order.id}")
+    @order = order
+    mail(to: "#{@order.email}", subject: "Order \##{@order.id}")
+    puts "it worked!"
   end
 end
