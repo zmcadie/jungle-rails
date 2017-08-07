7387c666520c6b6945b3bd26ec3c26479028ab44
Zola <zolamcadie@gmail.com>
2017-08-07 18:31:06 +0000

    added product spec/tests

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it 'should be valid with valid fields' do
+      @category = Category.new(name: 'new category')
+      @product = @category.products.new(name: 'new product',
+                                        price: 1000,
+                                        quantity: 3)
+      expect(@product).to be_valid
+    end
+
+    it 'should not be valid without name' do
+      @category = Category.new(name: 'new category')
+      @product = @category.products.new(name: nil,
+                                        price: 1000,
+                                        quantity: 3)
+      expect(@product).to_not be_valid
+    end
+
+    it 'should not be valid without price' do
+      @category = Category.new(name: 'new category')
+      @product = @category.products.new(name: 'new product',
+                                        price: nil,
+                                        quantity: 3)
+      expect(@product).to_not be_valid
+    end
+
+    it 'should not be valid without quantity' do
+      @category = Category.new(name: 'new category')
+      @product = @category.products.new(name: 'new product',
+                                        price: 1000,
+                                        quantity: nil)
+      expect(@product).to_not be_valid
+    end
+
+    it 'should not be valid without category' do
+      @product = Product.new(name: 'new product',
+                             price: 1000,
+                             quantity: 3,
+                             category: nil)
+      expect(@product).to_not be_valid
+    end
+  end
+end
