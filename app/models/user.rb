f1b0a15371cc94918536d766f4530490fa073eda
Zola <zolamcadie@gmail.com>
2017-08-07 19:40:05 +0000

    added password validation minimum length of 8 to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
 
   validates :first_name, :last_name, :email, :password, :password_confirmation, presence: true
   validates :email, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 8 }
 
 end
