faa863969442581e9a7de47d6438e40345c2fc88
Zola <zolamcadie@gmail.com>
2017-08-07 19:34:04 +0000

    updated user validations to add case insensitivity to email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :first_name, :last_name, :email, :password, :password_confirmation, presence: true
-  validates :email, uniqueness: true
+  validates :email, uniqueness: { case_sensitive: false }
 
 end
