b78767f5a378abe39e0a49bbaa57a10741960211
Zola <zolamcadie@gmail.com>
2017-08-07 18:47:15 +0000

    added valid user test, missing first name test, and missing last name test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should be valid with valid fields' do
+    @user = User.new(first_name: 'test',
+                     last_name: 'testington',
+                     email: 'user@user.com',
+                     password: 'password',
+                     password_confirmation: 'password')
+    expect(@user).to be_valid
+  end
+
+  it 'should not be valid without first_name' do
+    @user = User.new(first_name: nil,
+                     last_name: 'testington',
+                     email: 'user@user.com',
+                     password: 'password',
+                     password_confirmation: 'password')
+    expect(@user).to_not be_valid
+  end
+
+  it 'should not be valid without last_name' do
+    @user = User.new(first_name: 'test',
+                     last_name: nil,
+                     email: 'user@user.com',
+                     password: 'password',
+                     password_confirmation: 'password')
+    expect(@user).to_not be_valid
+  end
 end
