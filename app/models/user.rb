4ed35af9afdb7a87cd6c07ad3894ccaf908bf996
Zola <zolamcadie@gmail.com>
2017-08-07 20:14:13 +0000

    added best case scenario test for .authenticate_with_credentials class method in user spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 8 }
 
-  def self.authenticate_with_credentials(email, password)
+  def authenticate_with_credentials(email, password)
     @user = User.find_by_email(email)
     if @user.authenticate(password)
       @user
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,98 +1,111 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'should be valid with valid fields' do
-    @user = User.new(first_name: 'test',
-                     last_name: 'testington',
-                     email: 'user@user.com',
-                     password: 'password',
-                     password_confirmation: 'password')
-    expect(@user).to be_valid
+  describe 'Validations' do
+    it 'should be valid with valid fields' do
+      @user = User.new(first_name: 'test',
+                       last_name: 'testington',
+                       email: 'user@user.com',
+                       password: 'password',
+                       password_confirmation: 'password')
+      expect(@user).to be_valid
+    end
+
+    it 'should not be valid without first_name' do
+      @user = User.new(first_name: nil,
+                       last_name: 'testington',
+                       email: 'user@user.com',
+                       password: 'password',
+                       password_confirmation: 'password')
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to eql ["First name can't be blank"]
+    end
+
+    it 'should not be valid without last_name' do
+      @user = User.new(first_name: 'test',
+                       last_name: nil,
+                       email: 'user@user.com',
+                       password: 'password',
+                       password_confirmation: 'password')
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to eql ["Last name can't be blank"]
+    end
+
+    it 'should not be valid without email' do
+      @user = User.new(first_name: 'test',
+                       last_name: 'testington',
+                       email: nil,
+                       password: 'password',
+                       password_confirmation: 'password')
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to eql ["Email can't be blank"]
+    end
+
+    it 'should not be valid without password' do
+      @user = User.new(first_name: 'test',
+                       last_name: 'testington',
+                       email: 'user@user.com',
+                       password: nil,
+                       password_confirmation: 'password')
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to eql ["Password can't be blank", "Password can't be blank", "Password is too short (minimum is 8 characters)"]
+    end
+
+    it 'should not be valid without password' do
+      @user = User.new(first_name: 'test',
+                       last_name: 'testington',
+                       email: 'user@user.com',
+                       password: 'password',
+                       password_confirmation: nil)
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to eql ["Password confirmation can't be blank"]
+    end
+
+    it 'should not be valid if password and password_confirmation don\'t match' do
+      @user = User.new(first_name: 'test',
+                       last_name: 'testington',
+                       email: 'user@user.com',
+                       password: 'password',
+                       password_confirmation: 'passphrase')
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to eql ["Password confirmation doesn't match Password"]
+    end
+
+    it 'should not be valid if password length is less than 8 characters' do
+      @user = User.new(first_name: 'test',
+                       last_name: 'testington',
+                       email: 'user@user.com',
+                       password: 'pass',
+                       password_confirmation: 'pass')
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to eql ["Password is too short (minimum is 8 characters)"]
+    end
+
+    it 'should not be valid with duplicate email (case insensitive)' do
+      User.create!(first_name: 'test',
+                   last_name: 'testington',
+                   email: 'user@user.com',
+                   password: 'password',
+                   password_confirmation: 'password')
+      @user = User.new(first_name: 'test',
+                       last_name: 'testington',
+                       email: 'USER@USER.COM',
+                       password: 'password',
+                       password_confirmation: 'password')
+
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to eql ["Email has already been taken"]
+    end
   end
 
-  it 'should not be valid without first_name' do
-    @user = User.new(first_name: nil,
-                     last_name: 'testington',
-                     email: 'user@user.com',
-                     password: 'password',
-                     password_confirmation: 'password')
-    expect(@user).to_not be_valid
-    expect(@user.errors.full_messages).to eql ["First name can't be blank"]
-  end
-
-  it 'should not be valid without last_name' do
-    @user = User.new(first_name: 'test',
-                     last_name: nil,
-                     email: 'user@user.com',
-                     password: 'password',
-                     password_confirmation: 'password')
-    expect(@user).to_not be_valid
-    expect(@user.errors.full_messages).to eql ["Last name can't be blank"]
-  end
-
-  it 'should not be valid without email' do
-    @user = User.new(first_name: 'test',
-                     last_name: 'testington',
-                     email: nil,
-                     password: 'password',
-                     password_confirmation: 'password')
-    expect(@user).to_not be_valid
-    expect(@user.errors.full_messages).to eql ["Email can't be blank"]
-  end
-
-  it 'should not be valid without password' do
-    @user = User.new(first_name: 'test',
-                     last_name: 'testington',
-                     email: 'user@user.com',
-                     password: nil,
-                     password_confirmation: 'password')
-    expect(@user).to_not be_valid
-    expect(@user.errors.full_messages).to eql ["Password can't be blank", "Password can't be blank", "Password is too short (minimum is 8 characters)"]
-  end
-
-  it 'should not be valid without password' do
-    @user = User.new(first_name: 'test',
-                     last_name: 'testington',
-                     email: 'user@user.com',
-                     password: 'password',
-                     password_confirmation: nil)
-    expect(@user).to_not be_valid
-    expect(@user.errors.full_messages).to eql ["Password confirmation can't be blank"]
-  end
-
-  it 'should not be valid if password and password_confirmation don\'t match' do
-    @user = User.new(first_name: 'test',
-                     last_name: 'testington',
-                     email: 'user@user.com',
-                     password: 'password',
-                     password_confirmation: 'passphrase')
-    expect(@user).to_not be_valid
-    expect(@user.errors.full_messages).to eql ["Password confirmation doesn't match Password"]
-  end
-
-  it 'should not be valid if password length is less than 8 characters' do
-    @user = User.new(first_name: 'test',
-                     last_name: 'testington',
-                     email: 'user@user.com',
-                     password: 'pass',
-                     password_confirmation: 'pass')
-    expect(@user).to_not be_valid
-    expect(@user.errors.full_messages).to eql ["Password is too short (minimum is 8 characters)"]
-  end
-
-  it 'should not be valid with duplicate email (case insensitive)' do
-    User.create!(first_name: 'test',
-                 last_name: 'testington',
-                 email: 'user@user.com',
-                 password: 'password',
-                 password_confirmation: 'password')
-    @user = User.new(first_name: 'test',
-                     last_name: 'testington',
-                     email: 'USER@USER.COM',
-                     password: 'password',
-                     password_confirmation: 'password')
-
-    expect(@user).to_not be_valid
-    expect(@user.errors.full_messages).to eql ["Email has already been taken"]
+  describe '.authenticate_with_credentials' do
+    it 'should be valid when passed valid fields' do
+      @user = User.create!(first_name: 'test',
+                   last_name: 'testington',
+                   email: 'user@user.com',
+                   password: 'password',
+                   password_confirmation: 'password')
+      expect(@user.authenticate_with_credentials('user@user.com', 'password')).to eql @user
+    end
   end
 end
