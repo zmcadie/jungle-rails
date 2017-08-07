ac0e4871f283740547172f44b5388c691cc3882e
Zola <zolamcadie@gmail.com>
2017-08-07 19:59:01 +0000

    added authenticate_with_params user method and called it in sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 8 }
 
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by_email(email)
+    if @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
+
 end
