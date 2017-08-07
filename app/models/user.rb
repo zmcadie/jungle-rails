92ea8c07435e17db4429c5ff2cb1f1a6b66e11ab
Zola <zolamcadie@gmail.com>
2017-08-07 18:41:09 +0000

    added user spec file and updated user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :first_name, :last_name, :email, :password, :password_confirmation, presence: true
+  validates :email, uniqueness: true
+
 end
